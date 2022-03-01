9faebc84f696eb744639e6112c674e35bfecdbcd
haifabenhmida <haifa.benhamida@esprit.tn>
2022-03-01 15:50:14 +0100

    ajout commentaire

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -28,6 +28,7 @@ public class User implements Serializable {
 	private String firstName; 
 	
 	private String lastName;
+	//private String haifa;
 	
 	@Temporal(TemporalType.DATE)
 	private Date dateNaissance;
